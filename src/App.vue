63d8d130f6bd80057dcd2151a4486a3f409b3a1f
Ikhsan G <ikhsanguntara22@gmail.com>
2021-03-28 02:20:21 +0700

    navbar

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <v-app>
     <!-- Navigation Drawer -->
     <v-navigation-drawer v-model="drawer" app clipped>
+      <!-- items -->
       <v-list nav dense>
         <v-list-item-group v-model="item" color="primary">
           <div v-for="item in items" :key="item.title">
@@ -13,7 +14,11 @@
                 </v-list-item-content>
               </template>
 
-              <v-list-item v-for="subItem in item.items" :key="subItem.title" :to="subItem.link">
+              <v-list-item
+                v-for="subItem in item.items"
+                :key="subItem.title"
+                :to="subItem.link"
+              >
                 <v-list-item-icon>
                   <v-icon v-text="subItem.icon"></v-icon>
                 </v-list-item-icon>
@@ -35,7 +40,128 @@
           </div>
         </v-list-item-group>
       </v-list>
+      <v-divider></v-divider>
+      <!-- Data Master -->
+      <v-list nav dense>
+        <v-subheader>DATA MASTER</v-subheader>
+        <v-list-item-group v-model="item" color="primary">
+          <div v-for="item in masters" :key="item.title">
+            <!-- If Menu contains Sub Menu -->
+            <v-list-group v-if="item.items" :prepend-icon="item.icon">
+              <template v-slot:activator>
+                <v-list-item-content>
+                  <v-list-item-title v-text="item.title"></v-list-item-title>
+                </v-list-item-content>
+              </template>
 
+              <v-list-item
+                v-for="subItem in item.items"
+                :key="subItem.title"
+                :to="subItem.link"
+              >
+                <v-list-item-icon>
+                  <v-icon v-text="subItem.icon"></v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title v-text="subItem.title"></v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-group>
+
+            <!-- If Menu don't contains Sub Menu -->
+            <v-list-item v-else :to="item.link">
+              <v-list-item-icon>
+                <v-icon v-text="item.icon"></v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title v-text="item.title"></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </div>
+        </v-list-item-group>
+      </v-list>
+      <v-divider></v-divider>
+
+      <!-- transaction -->
+      <v-list nav dense>
+        <v-subheader>TRANSACTION</v-subheader>
+        <v-list-item-group v-model="item" color="primary">
+          <div v-for="item in transactions" :key="item.title">
+            <!-- If Menu contains Sub Menu -->
+            <v-list-group v-if="item.items" :prepend-icon="item.icon">
+              <template v-slot:activator>
+                <v-list-item-content>
+                  <v-list-item-title v-text="item.title"></v-list-item-title>
+                </v-list-item-content>
+              </template>
+
+              <v-list-item
+                v-for="subItem in item.items"
+                :key="subItem.title"
+                :to="subItem.link"
+              >
+                <v-list-item-icon>
+                  <v-icon v-text="subItem.icon"></v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title v-text="subItem.title"></v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-group>
+
+            <!-- If Menu don't contains Sub Menu -->
+            <v-list-item v-else :to="item.link">
+              <v-list-item-icon>
+                <v-icon v-text="item.icon"></v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title v-text="item.title"></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </div>
+        </v-list-item-group>
+      </v-list>
+      <v-divider></v-divider>
+      <!-- User -->
+      <v-list nav dense>
+        <v-subheader>SETTINGS</v-subheader>
+        <v-list-item-group v-model="item" color="primary">
+          <div v-for="item in users" :key="item.title">
+            <!-- If Menu contains Sub Menu -->
+            <v-list-group v-if="item.items" :prepend-icon="item.icon">
+              <template v-slot:activator>
+                <v-list-item-content>
+                  <v-list-item-title v-text="item.title"></v-list-item-title>
+                </v-list-item-content>
+              </template>
+
+              <v-list-item
+                v-for="subItem in item.items"
+                :key="subItem.title"
+                :to="subItem.link"
+              >
+                <v-list-item-icon>
+                  <v-icon v-text="subItem.icon"></v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title v-text="subItem.title"></v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-group>
+
+            <!-- If Menu don't contains Sub Menu -->
+            <v-list-item v-else :to="item.link">
+              <v-list-item-icon>
+                <v-icon v-text="item.icon"></v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title v-text="item.title"></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </div>
+        </v-list-item-group>
+      </v-list>
+      <v-divider></v-divider>
       <template v-slot:append>
         <div class="pa-2">
           <v-btn outlined block color="accent">
@@ -91,22 +217,46 @@ export default {
       {
         title: "Dashboard",
         icon: "mdi-view-dashboard",
-        link: "/dashboard"
-      
+        link: "/dashboard",
       },
+    ],
+    masters: [
+      {
+        title: "Supplier",
+        icon: "mdi-account-group",
+      },
+      {
+        title: "Items",
+        icon: "mdi-folder",
+        items: [
+          { title: "Satuan Barang", icon: "mdi-file", link: "" },
+          { title: "Jenis Barang", icon: "mdi-file", link: "" },
+          { title: "Data Barang", icon: "mdi-file", link: "" },
+        ],
+      },
+    ],
+    transactions: [
       {
         title: "Transactions",
         icon: "mdi-view-dashboard",
         items: [
-          { title: "Items In", icon: "mdi-chart-line", link: "/stock_in"},
-          { title: "Items Out", icon: "mdi-chart-line",link: "/stock_out"},
-          
-          ],
+          { title: "Items In", icon: "mdi-download", link: "/stock_in" },
+          { title: "Items Out", icon: "mdi-upload", link: "/stock_out" },
+        ],
+      },
+      {
+        title: "Report",
+        icon: "mdi-printer",
+      },
+    ],
+    users: [
+      {
+        title: "User Management",
+        icon: "mdi-account-plus",
       },
     ],
   }),
 };
 </script>
 
-<style>
-</style>
+<style></style>
